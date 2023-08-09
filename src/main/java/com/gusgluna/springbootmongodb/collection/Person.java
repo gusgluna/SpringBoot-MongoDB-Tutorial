0d18bf796e5e7e0edcce93bdcea7401fc277cd48
gusgluna <gusgluna@gmail.com>
2023-08-08 21:55:10 -0600

    Creating Address package

---
+++ src/main/java/com/gusgluna/springbootmongodb/collection/Person.java
@@ -0,0 +1,12 @@
+package com.gusgluna.springbootmongodb.collection;
+
+import java.util.List;
+
+public class Person {
+    private String personId;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private List<String> hobbies;
+    private List<Address> addresses;
+}
