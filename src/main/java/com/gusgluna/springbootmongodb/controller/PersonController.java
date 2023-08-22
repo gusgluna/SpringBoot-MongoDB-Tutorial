148bd93b5904938d2e511507e4cf6b5a782e34ba
gusgluna <gusgluna@gmail.com>
2023-08-21 21:42:35 -0600

    implement /search end pont with pagination

--- src/main/java/com/gusgluna/springbootmongodb/controller/PersonController.java
+++ src/main/java/com/gusgluna/springbootmongodb/controller/PersonController.java
@@ -3,6 +3,9 @@ package com.gusgluna.springbootmongodb.controller;
 import com.gusgluna.springbootmongodb.collection.Person;
 import com.gusgluna.springbootmongodb.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,4 +35,17 @@ public class PersonController {
     public List<Person> getByPersonAge(@RequestParam("minAge") Integer minAge, @RequestParam("maxAge") Integer maxAge){
         return personService.getByPersonAge(minAge, maxAge);
     }
+
+    @GetMapping("/search")
+    public Page<Person> searchPerson(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer minAge,
+            @RequestParam(required = false) Integer maxAge,
+            @RequestParam(required = false) String city,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "5") Integer size
+    ){
+        Pageable pageable = PageRequest.of(page, size);
+        return  personService.search(name, minAge, maxAge, city, pageable);
+    };
 }
